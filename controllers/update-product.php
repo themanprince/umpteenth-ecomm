d51c4ee3a735ca6d3f52c2b4575e9b8a036db6fd
themanprince <princeadigwe29@gmail.com>
2025-06-21 00:41:26 +0100

    nothing serious.. just that the frontend for updating/editing products is ready

---
+++ controllers/update-product.php
@@ -0,0 +1,4 @@
+<?php
+    echo("got to update-product.php, what was posted is");
+    print_r($_POST);
+?>
--- views/view-product.php
+++ views/view-product.php
@@ -1,5 +1,6 @@
 <?php
     require("../database.php");
+    include("../general-config.php");
     $db = new Database;
 ?>
 
@@ -9,16 +10,77 @@
         <?php
             include("./title-and-meta.php");
         ?>
+        <style>
+            .product-image {
+                inline-size: 90%;
+                border-radius: 10%;
+                block-size: 30%;
+                margin-inline: auto;
+                margin-block: 20px;
+            }
+            .product-image img {
+                max-inline-size: 100%;
+            }
+        </style>
     </head>
     <body>
         <?php
             if(isset($_GET["product_id"])) {
-                $product = $db -> db_getonerow("SELECT * FROM products WHERE product_id = " . $_GET['product_id']);
-                echo("got here, product is");
-                print_r($product);
+                $product_properties = $db -> db_getonerow("SELECT * FROM products WHERE product_id = " . $_GET['product_id']);
+                if (isset($product_properties)) {
+                    echo("<div class='product-image'><img src='" . $product_properties['product_image_url'] ."'/></div>");
         ?>
-
-        <?php
+                    <form action="../controllers/update-product.php" method="POST">
+                        <div class="container w-75 container-sm-fluid mx-auto">
+                            <h2>Edit Product</h2>
+                            <div class="row" id="form-fields-container">
+                                <?php
+                                    $keys_to_avoid = array("product_id", "product_image_url", "product_description", "is_hidden");
+                                    foreach ($product_properties as $key => $value) {
+                                        if (in_array($key, $keys_to_avoid)) {
+                                            continue;
+                                        } #else...
+                                ?>
+                                        <div class="col-12 my-3 my-sm-2">
+                                            <label class="form-label" ><?php echo($key); ?></label><br/>
+                                            <input name="<?php echo($key); ?>" class="form-control" type="<?php echo((is_numeric($value)) ? "number": "text"); ?>" value="<?php echo($value); ?>"/>
+                                        </div>
+                                <?php
+                                    }
+                                ?>
+                                <input type="hidden" value="<?php echo($product_properties['product_id']) ?>" name="product_id" />
+                                <div class="col-12 my-3 my-sm-2">
+                                    <label class="form-label" >product_description</label><br/>
+                                    <textarea name="product_description" class="form-control"></textarea>
+                                </div>
+                                <div class="col-12 my-3 my-sm-2">
+                                    <label class="form-label">product_image</label>
+                                    <input type="file" name="product_image" class="form-control"/>
+                                </div>
+                                <div class="col-12 my-3 my-sm-2">
+                                    <label class="form-label">is_hidden</label>
+                                    <select name="is_hidden" class="form-control">
+                                        <option value="TRUE">Yes</option>
+                                        <option value="FALSE">No</option>
+                                    </select>
+                                </div>
+                                    
+                                <div class="col-6">
+                                    <button type="submit" class="btn btn-primary">Update Product Details</button>
+                                </div>
+                                <div class="col-6">
+                                    <a class="btn btn-danger" onclick="sendDeleteRequest(<?php echo($product_properties['product_id']); ?>)">Delete Product</a>
+                                </div>                                
+                            </div>
+                        </div>                       
+                        
+                    </form>
+            <?php
+                } else {
+                    echo("Error! This product could not be retrieved");
+                    die();
+                }
+        
             } else {
                 $products_list = $db -> db_queryresult("SELECT * FROM products;");
         ?>
@@ -26,8 +88,7 @@
             <table class="table table-primary table-striped table-bordered">
                 <thead>
                     <?php
-                        $keys_to_avoid = array("product_id", "product_image_url");
-                        
+                        $keys_to_avoid = array("product_id", "product_image_url");                        
                         foreach ($products_list[0] as $key => $value) {
                             if (in_array($key, $keys_to_avoid)) {
                                 continue;
@@ -63,5 +124,16 @@
                     ?>                    
                 </tbody>
             </table> 
+        <script>
+            const sendDeleteRequest = (product_id) => async () => {
+                const requestArgs = {
+                    "method": "POST",
+                    "body": JSON.stringify({product_id})
+                };
+                const response = await fetch("../controllers/delete-product.php", requestArgs);
+                if(response.status != 200)
+                    window.alert("unable to delete product");
+            };
+        </script>
     </body>
 </html>
