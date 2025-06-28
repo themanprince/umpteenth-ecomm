0631ec6929212a21be5dc5ac08834414589b8898
themanprince <princeadigwe29@gmail.com>
2025-06-28 17:26:07 +0100

    add a detailed view for each order

--- views/order-view.php
+++ views/order-view.php
@@ -13,6 +13,49 @@
     </head>
     <body>
         <?php
+            if (isset($_GET["order_id"])) {
+                $order_id = $_GET["order_id"];
+                $order_details = $db -> db_getonerow("SELECT * FROM orders WHERE order_id=" . $order_id . ";");
+                $ordered_items = $db -> db_queryresult("SELECT oi.price, oi.quantity_purchased, p.product_name, p.product_image_url FROM ordered_items oi JOIN products p ON p.product_id=oi.product_id WHERE oi.order_id=" . $order_id . ";");
+
+                $total = 0;
+        ?>
+        <div class="container py-4">
+            <h4>Order For <?php echo($order_details["customer_name"]); ?></h4>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Qty Purchased</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody id="order-items">
+                    <?php
+                        foreach ($ordered_items as $item) {
+                            $item_total = $item["quantity_purchased"] * $item["price"];
+                            $total += $item_total;
+                    ?>
+                    <tr>
+                        <td><img src="<?php echo($item['product_image_url']); ?>" width="50"/> <?php echo($item["product_name"]); ?> </td>
+                        <td><?php echo($item["quantity_purchased"]); ?></td>
+                        <td>N<?php echo($item["price"]); ?></td>
+                        <td>N<?php echo($item_total); ?></td>
+                    </tr>
+                    <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
+            <div class="text-end fw-bold" id="order-total"><?php echo($total); ?></div>
+            <div class="container">
+                <div class="col-12">
+                    <?php echo((($order_details['is_completed'] == "1")? "Order Completed" : "<a class='btn btn-secondary' href='../controllers/mark-order-complete.php?order_id=" . $order_id . "'>Mark as Completed</a>")); ?>
+                </div>
+            </div>
+        <?php
+            } else {
             $orders_list = $db -> db_queryresult("SELECT * FROM orders;");
         ?>
             <div class="w-100 bg-info text-light text-center py-3 h2">Orders</div>
@@ -56,5 +99,8 @@
                     </tbody>
                 </table>
             </div>
+        <?php
+            }
+        ?>
     </body>
 </html>
