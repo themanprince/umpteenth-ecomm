2509fc25170329e01e77214b793b8d5644fd74e0
themanprince <princeadigwe29@gmail.com>
2025-07-01 22:55:58 +0100

    before I add banner section to UI

--- site_config.php
+++ site_config.php
@@ -3,6 +3,7 @@
     $site_name = "prechito_collections";
     $site_tagline = "ahead of the hype";
     $site_logo_url = "icons/favico.jpg"; #as of now, site icon needs to be a local image stored on the server
+    $homepage_banner_img = "banner-img.jpg";
 
     $product_out_of_stock_text = "Out of Stock";
 
---
+++ views/components/banner.php
@@ -0,0 +1,22 @@
+<?php require_once("../site_config.php"); ?>
+
+<section class="banner_part">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-5">
+                    <div class="banner_text">
+                        <div class="banner_text_iner">
+                            <h1><?php echo($site_name); ?></h1>
+                            <p><?php echo($site_tagline); ?></p>
+                            <a href="#product_list" class="btn_1">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="banner_img">
+            <img src="<?php echo($homepage_banner_img_url); ?>" alt="#" class="img-fluid">
+            <img src="../banner_pattern.png " alt="#" class="pattern_img img-fluid">
+        </div>
+    </section>
+    
