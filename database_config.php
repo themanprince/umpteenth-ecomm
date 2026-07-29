a32f509c0402be4f3bbde253a6ddf8e972d8f8e9
themanprince <princeadigwe29@gmail.com>
2026-07-29 07:40:26 +0100

    update db config

--- database_config.php
+++ database_config.php
@@ -1,7 +1,7 @@
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_selected = "umpteenth_ecomm";
-    $db_port = 3306;
+    $db_server = "gateway01.sa-east-1.prod.aws.tidbcloud.com";
+    $db_user = "22smL4cqCUdaW69.root";
+    $db_password = "iPhG4hfUewLPaUQp";
+    $db_selected = "sys";
+    $db_port = 4000;
 ?>
