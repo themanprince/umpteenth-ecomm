8c8e8e4a6459c0b6487bf999f30c3fd0eb628cba
themanprince <princeadigwe29@gmail.com>
2026-07-30 20:35:24 +0100

    DB and tables creation/seeding script update

--- database_exports/umpteenth_ecomm.sql
+++ database_exports/umpteenth_ecomm.sql
@@ -24,52 +24,28 @@ USE `umpteenth_ecomm`;
 -- Creation: Jun 20, 2025 at 05:13 PM
 -- Updated: July 30, 2026 at 7:15am
 CREATE TABLE IF NOT EXISTS `products` (
-  `product_id` bigint(20) UNSIGNED NOT NULL,
+  `product_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `product_name` text NOT NULL,
   `product_price` float NOT NULL,
   `product_description` text NOT NULL,
   `product_image_url` text NOT NULL,
   `product_quantity_avail` float NOT NULL,
-  `is_hidden` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'From feature spec I got, Admin can choose to hide a product from appearing on Customers'' end'
+  `is_hidden` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'From feature spec I got, Admin can choose to hide a product from appearing on Customers'' end',
+  PRIMARY KEY (product_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`),
-  ADD UNIQUE KEY `product_id` (`product_id`);
-
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `product_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
 
 
 -- Creation: Jun 28, 2025 at 02:36 PM
 -- Updated: Jul 30, 2026 at 7:	6am
 CREATE TABLE IF NOT EXISTS `orders` (
-  `order_id` bigint(20) UNSIGNED NOT NULL,
+  `order_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `customer_name` text NOT NULL,
   `customer_email` text NOT NULL,
   `customer_address` text NOT NULL,
   `customer_phone_number` text NOT NULL,
-  `is_completed` tinyint(1) NOT NULL DEFAULT 0
+  `is_completed` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (order_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='for storing orders';
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD UNIQUE KEY `order_id` (`order_id`);
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 
 
@@ -79,21 +55,11 @@ CREATE TABLE IF NOT EXISTS `ordered_items` (
   `order_id` bigint(20) UNSIGNED NOT NULL,
   `product_id` bigint(20) UNSIGNED NOT NULL,
   `price` float NOT NULL COMMENT 'what was the price of the product at the time of purchase',
-  `quantity_purchased` int(11) NOT NULL DEFAULT 1
+  `quantity_purchased` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (order_id, product_id),
+  CONSTRAINT `ordered_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  CONSTRAINT `ordered_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='this is a list of products ordered per each order';
-
---
--- Indexes for table `ordered_items`
---
-ALTER TABLE `ordered_items`
-  ADD PRIMARY KEY (`order_id`,`product_id`),
-  ADD KEY `product_id` (`product_id`);
-
--- Constraints for table `ordered_items`
---
-ALTER TABLE `ordered_items`
-  ADD CONSTRAINT `ordered_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
-  ADD CONSTRAINT `ordered_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
