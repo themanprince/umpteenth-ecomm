181e0f5af0590624506babd1a39eb9d3c5019b25
themanprince <princeadigwe29@gmail.com>
2026-07-30 19:56:06 +0100

    changed DB used

--- database_config.php
+++ database_config.php
@@ -2,6 +2,6 @@
     $db_server = "gateway01.sa-east-1.prod.aws.tidbcloud.com";
     $db_user = "22smL4cqCUdaW69.root";
     $db_password = "iPhG4hfUewLPaUQp";
-    $db_selected = "sys";
+    $db_selected = "umpteenth_ecomm";
     $db_port = 4000;
 ?>
