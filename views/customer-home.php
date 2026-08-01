fb5669d1bbfc1646e4f442e79f84d9d83e4d458f
themanprince <princeadigwe29@gmail.com>
2026-08-01 18:32:16 +0100

    seem to have seen the cause icons not displaying... the svg icons are not displayable

--- views/customer-home.php
+++ views/customer-home.php
@@ -38,25 +38,25 @@
                 <div class="row justify-content-center">
                     <div class="col-lg-3 col-sm-6">
                         <div class="single_feature_part">
-                            <img src="/icons/feature_icon_1.svg" alt="#">
+                            <img src="/icons/feature_icon_1.png" alt="#">
                             <h4>Credit Card Support</h4>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="single_feature_part">
-                            <img src="/icons/feature_icon_2.svg" alt="#">
+                            <img src="/icons/feature_icon_2.png" alt="#">
                             <h4>Online Order</h4>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="single_feature_part">
-                            <img src="/icons/feature_icon_3.svg" alt="#">
+                            <img src="/icons/feature_icon_3.png" alt="#">
                             <h4>Fast Delivery</h4>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="single_feature_part">
-                            <img src="/icons/feature_icon_4.svg" alt="#">
+                            <img src="/icons/feature_icon_4.png" alt="#">
                             <h4>Product with Gift</h4>
                         </div>
                     </div>
